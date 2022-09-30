465ced39e5a251113220509b093580fc91cb9d82
Kazybek <seidakhmetov.kazybek@gmail.com>
2022-09-30 17:17:34 +0600

    19. Практика

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-"use strict";
-const category = 'toys';
-console.log(`https://somurl.com/${category}/5`);
-
-const user = 'Kazybek';
-alert(`Hello, ${user}`);
-
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотенных фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+console.log(personalMovieDB);
